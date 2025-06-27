1c638777bbcca09868a8caf73aa5ccd4cdff1674
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-06-27 13:50:01 +0200

    fix bugs

--- src/app/components_settings/test.tsx
+++ src/app/components_settings/test.tsx
@@ -2,19 +2,20 @@
 
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
+
+
 export const Test = ()=>{
    
 
     return(
-        <div className='flex  flex-col '>
-        <h1 className='text-center mt-[80px] mb-[80px] text-2xl'>Ups....  this page is not ready yet</h1>
-        <div>
-            <DotLottieReact
+        
+        <div className="flex flex-col items-center py-20">
+    <h1 className="text-center text-2xl mb-6">Oops… This page isn’t ready yet</h1>
+     <DotLottieReact
       src="https://lottie.host/ffc0444c-c99d-4337-b3c9-47b71cbd4033/s04ymTMzCO.lottie"
       loop
       autoplay
     />
-</div>
-        </div>
+  </div>
     )
 }
