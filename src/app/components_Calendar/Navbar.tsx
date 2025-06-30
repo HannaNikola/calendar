8560c855cd5137668df63aa7ea9d5f8a199b6577
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-06-30 12:03:43 +0200

    fix bug

--- src/app/components_Calendar/Navbar.tsx
+++ src/app/components_Calendar/Navbar.tsx
@@ -11,19 +11,19 @@ export const Navbar = ({className}: NavbarProps) => {
   return (
     <section className="flex justify-center">
     <ul className=" lg:mr-15 flex  flex-col  w-[260px] mb-[40px] ">
-      <li className=" flex  px-[40px] py-[5px] bg-navbar-button transition-colors duration-500 hover:bg-navbar-button-hover rounded-sm mb-2 items-center shadow-sm">
+      <li className=" flex  pl-[40px] pr-[120px] py-[5px] bg-navbar-button transition-colors duration-500 hover:bg-navbar-button-hover rounded-sm mb-2 items-center shadow-sm">
         <CalendarDays size={20} />
         <Link className="ml-3 text-main" href="/">
           Calendar
         </Link>
       </li>
-      <li className=" flex px-[40px] py-[5px] bg-navbar-button transition-colors duration-500 hover:bg-navbar-button-hover rounded-sm mb-2 items-center shadow-sm">
+      <li className=" flex pl-[40px] pr-[120px] py-[5px] bg-navbar-button transition-colors duration-500 hover:bg-navbar-button-hover rounded-sm mb-2 items-center shadow-sm">
         <BookOpenCheck size={20} />
         <Link className="ml-3 text-main" href="/task">
           Your Task
         </Link>
       </li>
-      <li className=" flex  bg-navbar-button  px-[40px] py-[5px] transition-colors duration-500 hover:bg-navbar-button-hover rounded-sm items-center shadow-sm">
+      <li className=" flex  bg-navbar-button  pl-[40px] pr-[120px] py-[5px] transition-colors duration-500 hover:bg-navbar-button-hover rounded-sm items-center shadow-sm">
         <Cog size={20} />
         <Link className="ml-3 text-main" href="/settings">
           Settings
