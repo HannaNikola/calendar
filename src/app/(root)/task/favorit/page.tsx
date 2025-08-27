e5bcbd9ad0d17433471f9faf8b8bc57fff441afc
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-08-27 22:38:25 +0200

    routs

---
+++ src/app/(root)/task/favorit/page.tsx
@@ -0,0 +1,25 @@
+import { Test } from "@/app/components_settings/test";
+import { BookOpenCheck, CircleCheck, Star } from "lucide-react";
+import Link from "next/link";
+
+export default function Favorite() {
+  return (
+    <div className="w-full ">
+      <div className="flex  gap-5 mt-2 justify-center">
+        <Link href="/task/favorit">
+          <Star size={20} />
+        </Link>
+        <Link href="/task/completed">
+          <CircleCheck size={20} />
+        </Link>
+        <Link href="/task">
+          <BookOpenCheck size={20} />
+        </Link>
+      </div>
+
+      <div className="  flex  flex-col items-center justify-center py-10">
+        <Test />
+      </div>
+    </div>
+  );
+}
--- src/app/(root)/task/page.tsx
+++ src/app/(root)/task/page.tsx
@@ -1,11 +1,21 @@
 import { TaskEl } from "@/app/components_Task/Task";
-
-
+import { CircleCheck, Star } from "lucide-react";
+import Link from "next/link";
 
 export default function Task() {
   return (
+    <div className="flex flex-col w-full ">
+      <div className="flex flex-row gap-5 mb-3 justify-center">
+        <Link href="/task/favorit">
+          <Star />
+        </Link>
+        <Link href="/task/completed">
+          <CircleCheck />
+        </Link>
+      </div>
     <div className="flex w-full flex-col lg:flex-row">
       <TaskEl />
+      </div>
     </div>
   );
 }
