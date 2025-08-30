c3f87b1f19a62b244f3cb5f20e95ae27c5a0b418
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-08-30 12:53:23 +0200

    completed_filter

--- src/app/(root)/task/completed/page.tsx
+++ src/app/(root)/task/completed/page.tsx
@@ -1,4 +1,5 @@
-import { Test } from "@/app/components_settings/test";
+
+import { TodoCompleted } from "@/app/components_Task/TodoCompleted";
 import { BookOpenCheck, CircleCheck, Star } from "lucide-react";
 import Link from "next/link";
 
@@ -7,7 +8,7 @@ export default function Completed() {
     <div className="w-full">
       <div className="flex justify-center gap-5 mt-2 ">
         <Link href="/task/favorit">
-          <Star size={20} />
+          <Star size={20} className="fill-amber-300"/>
         </Link>
         <Link href="/task/completed">
           <CircleCheck size={20} />
@@ -17,8 +18,8 @@ export default function Completed() {
         </Link>
       </div>
 
-      <div className="  flex  flex-col items-center  py-10">
-        <Test />
+      <div className="  flex w-full  flex-col items-center  py-10">
+        <TodoCompleted />
       </div>
     </div>
   );
