06f6be3d9ebe857f65060ba3a4e84deeb7560cb9
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-09-18 22:03:25 +0200

    refactoring_filter

--- src/app/components_Task/TaskList.tsx
+++ src/app/components_Task/TaskList.tsx
@@ -1,24 +1,23 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTodosApi } from "../api/todoApi";
 import { AppDispatch, RootState } from "../store/store";
 import { ModalTodo } from "./ModalTodo";
 import { closeElementModal } from "../store/sharedComponent/modalReducer";
-
 import { TaskItem } from "./TaskItem";
-
+import { selectFilterResult } from "../store/filters/selector";
+import { CalendarTodo } from "../types/typesTodoApi";
 
 export const TaskList = () => {
   const dispatch = useDispatch<AppDispatch>();
-
-  const { todos, status } = useSelector((state: RootState) => state.todo);
-  const { isOpen, type, selectedId } = useSelector(
-    (state: RootState) => state.modal
-  );
+  const { status } = useSelector((state: RootState) => state.todo);
+  const { isOpen, type } = useSelector((state: RootState) => state.modal);
 
   const [showLoader, setShowLoader] = useState(false);
-
+  const queryResult = useSelector(selectFilterResult);
+  const entity = useSelector((state: RootState) => state.filter.entity);
 
   useEffect(() => {
     if (status === "loading") {
@@ -32,20 +31,24 @@ export const TaskList = () => {
   useEffect(() => {
     dispatch(fetchTodosApi());
   }, [dispatch]);
-const activeTodos = todos.filter((item) => !item.isCompleted);
+
+  const activeTodos =
+    entity === "todo"
+      ? (queryResult as CalendarTodo[]).filter((item) => !item.isCompleted)
+      : [];
 
   return (
     <>
       <div className="flex w-full flex-col ">
         {showLoader ? (
-           <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="w-12 h-12 border-4 border-blue-400 border-dashed rounded-full border-t-transparent animate-spin" />
-        </div>
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="w-12 h-12 border-4 border-blue-400 border-dashed rounded-full border-t-transparent animate-spin" />
+          </div>
         ) : (
           <ul className="flex w-full flex-col">
-            {
-              activeTodos.map((item) => <TaskItem key={item._id} item={item} />)
-            }
+            {activeTodos.map((item) => (
+              <TaskItem key={item._id} item={item} />
+            ))}
           </ul>
         )}
       </div>
--- src/app/store/filters/selector.ts
+++ src/app/store/filters/selector.ts
@@ -2,13 +2,10 @@ import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import Fuse from "fuse.js";
 
-
-export const selectFilterQuery = (state:RootState)=> state.filter.query;
-export const selectFilterEntity = (state:RootState)=> state.filter.entity;
-export const selectAllEvents = (state:RootState)=> state.eventData.events;
-export const selectAllTodo = (state:RootState)=> state.todo.todos;
-
-
+export const selectFilterQuery = (state: RootState) => state.filter.query;
+export const selectFilterEntity = (state: RootState) => state.filter.entity;
+export const selectAllEvents = (state: RootState) => state.eventData.events;
+export const selectAllTodo = (state: RootState) => state.todo.todos;
 
 export const selectFilteredEvents = createSelector(
   [selectFilterQuery, selectFilterEntity, selectAllEvents],
@@ -16,37 +13,31 @@ export const selectFilteredEvents = createSelector(
     if (entity !== "event" || !query) return events;
 
     const fuse = new Fuse(events, {
-      keys: ["title"], 
-      threshold: 0.4, 
+      keys: ["title"],
+      threshold: 0.4,
     });
 
     const results = fuse.search(query);
-    return results.map(result => result.item);
-    
+    return results.map((result) => result.item);
   }
 );
 
 export const selectFilteredTodos = createSelector(
   [selectFilterQuery, selectFilterEntity, selectAllTodo],
-(query, entity, todos)=>{
-  if(entity !== 'todo' || !query) return todos
+  (query, entity, todos) => {
+    if (entity !== "todo" || !query) return todos;
 
-  const fuse = new Fuse(todos, {keys: ["title"],
-  threshold: 0.4})
-  const results = fuse.search(query)
-  return results.map(result => result.item)
-   
-}
-
-)
+    const fuse = new Fuse(todos, { keys: ["title"], threshold: 0.4 });
+    const results = fuse.search(query);
+    return results.map((result) => result.item);
+  }
+);
 
 export const selectFilterResult = createSelector(
-  [ selectFilterEntity,  selectFilteredEvents, selectFilteredTodos],
-  (entity, filteredEvents, filteredTodos)=>{
-    if(entity === 'event') return filteredEvents;
-    if(entity === 'todo') return filteredTodos;
-    return []
+  [selectFilterEntity, selectFilteredEvents, selectFilteredTodos],
+  (entity, filteredEvents, filteredTodos) => {
+    if (entity === "event") return filteredEvents;
+    if (entity === "todo") return filteredTodos;
+    return [];
   }
-)
-
-
+);
