fd9fc0d0bdfd0c57c321c6f96476843c3ad255e0
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-10-13 23:15:50 +0200

    addButton

--- src/app/types/typesApi.ts
+++ src/app/types/typesApi.ts
@@ -1,18 +1,22 @@
+
 export interface CalendarEvent {
   _id?: string;
-  title: string;
+  title: string; 
   description?: string;
-  addTask?: boolean;
-  repeat?: "none" | "daily" | "weekday" | "weekend"; // заглушка
-  reminder?: {
-    triggerBefore?: "30min" | "1hour" | "1day" | "none"; // заглушка
-    notifyAt?: Date | null;
-    notified?: boolean;
-  };
   start?: string | Date | null;
   end?: string | Date | null;
   allDay?: boolean;
+  addTask?: boolean;
+  todoId?: string | null; 
+  colorEvent?: "none" | "home" | "work" | "isektor";
+  repeat?: "none" | "daily" | "workday" | "weekend";
+  reminder?: {
+    triggerBefore?: "30min" | "1hour" | "1day" | "none";
+    notifyAt?: Date | null;
+    notified?: boolean;
+  };
 }
+
 
 export interface EventStateProps {
   events: CalendarEvent[];
