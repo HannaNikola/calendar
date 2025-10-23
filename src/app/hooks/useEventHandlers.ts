c017ddff97f2e6da0ee52dfd41a8fadf35316e55
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-10-23 23:59:36 +0200

    Create_task

--- src/app/hooks/useEventHandlers.ts
+++ src/app/hooks/useEventHandlers.ts
@@ -129,3 +129,9 @@ export const useEventHandlers = () => {
     handleDeleteEvent,
   };
 };
+
+
+
+
+
+
--- src/app/shared/ui/ModalWrapper.tsx
+++ src/app/shared/ui/ModalWrapper.tsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import { useEffect, useState } from "react";
 
 interface ModalWrapperProps {
   isOpen: boolean;
@@ -17,6 +18,23 @@ export const ModalWrapper = ({
 }: ModalWrapperProps) => {
   if (!isOpen) return null;
 
+  const [visible, setIsVisible] = useState(isOpen);
+  const [animate, setanimAte] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true);
+      requestAnimationFrame(() => setanimAte(true));
+    } else {
+      setanimAte(false);
+      const timer = setTimeout(() => setIsVisible(true), 300);
+      return () => clearTimeout(timer);
+    }
+    [isOpen];
+  });
+
+  if (!visible) return null;
+
   const handelOverlowClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
       onClose();
@@ -25,10 +43,14 @@ export const ModalWrapper = ({
   return (
     <div
       onClick={handelOverlowClick}
-      className="fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-50 p-4"
+      className={`fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-50 p-4 transition-opacity duration-300
+         ${animate ? "bg-black/50 opacity-100" : "opacity-0"}`}
     >
       <div
-        className={`relative bg-white rounded-lg shadow-lg  p-4 animate-fadeIn ${className}`}
+        className={`relative bg-white rounded-lg shadow-lg  p-4 transform transition-all duration-300
+          ${animate ? "scale-100 opacity-100" : "scale-95 opacity-0"
+        }
+           ${className}`}
       >
         {withCloseButton && (
           <button
