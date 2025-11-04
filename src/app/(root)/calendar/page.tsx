84d060edfd67307d6151de15ef2c3e9392fb7a49
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-11-04 16:29:58 +0100

    task_list

--- src/app/(root)/calendar/page.tsx
+++ src/app/(root)/calendar/page.tsx
@@ -1,13 +1,24 @@
+"use client"
 import CalendarEl from "@/app/components_Calendar/Calendar";
+import { Tasklistsection } from "@/app/components_Calendar/Tasklistsection";
+import { useScreenType } from "@/app/hooks/useScreenType";
 
 
 export default function Calendar() {
+  const screenType = useScreenType();
  
   return (
     <div className="flex flex-col lg:flex-row  ">
       <div className=" flex h-full flex-col overflow-y-auto ">
       <CalendarEl />
       </div>
+      
+      {screenType === 'desktop' &&(
+        <div className="flex h-full w-[260px]">
+        <Tasklistsection/>
+      </div>
+      )}
+      
      </div>
  );
 }
