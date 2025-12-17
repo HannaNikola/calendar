7cb1c06769bab77d5b5ad73a2c103a22d82261fe
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-12-17 16:24:05 +0100

    update_next_config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,27 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     reactCompiler: true 
+//   },
+//   i18n: {
+//      locales: ["en"],
+//      defaultLocale: "en",
+//     },
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true 
-  },
   i18n: {
-     locales: ["en"],
-     defaultLocale: "en",
-    },
-  /* config options here */
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
 
 export default nextConfig;
-
-
-
